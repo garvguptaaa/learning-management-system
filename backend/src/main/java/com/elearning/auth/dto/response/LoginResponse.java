062e4338d007ad91289724a3d077f84b5608db66
garv gupta <garvgupta1503@gmail.com>
2026-07-21 11:32:59 +0530

    feat(auth): add login response DTO

--- backend/src/main/java/com/elearning/auth/dto/response/LoginResponse.java
+++ backend/src/main/java/com/elearning/auth/dto/response/LoginResponse.java
@@ -2,20 +2,33 @@ package com.elearning.auth.dto.response;
 
 import java.util.Set;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponse {
+
+    private boolean success;
+
+    private String message;
+
+    private String token;
 
     private Long id;
 
-    private String fullName;
+    private String firstName;
+
+    private String lastName;
 
     private String email;
 
     private Set<String> roles;
 
-    private String message;
 }
