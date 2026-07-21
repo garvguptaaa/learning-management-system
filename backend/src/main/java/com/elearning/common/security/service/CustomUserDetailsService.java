f9088617b9c9d2b4a29eee9c555cdd89e22d6962
garv gupta <garvgupta1503@gmail.com>
2026-07-21 11:08:47 +0530

    feat(security): implement CustomUserDetailsService

---
+++ backend/src/main/java/com/elearning/common/security/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.elearning.common.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.elearning.common.security.model.CustomUserDetails;
+import com.elearning.user.entity.User;
+import com.elearning.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found with email: " + email));
+
+        return new CustomUserDetails(user);
+    }
+}
